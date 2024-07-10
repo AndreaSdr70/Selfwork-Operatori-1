6af96ddeba38aa973f021b903ec7de7322a7025a
AndreaSdr70 <andrea@sdringola.it>
2024-07-11 00:09:10 +0200

    Selfwork Operatori 1

---
+++ main.js
@@ -0,0 +1,13 @@
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+let n4 = 4;
+let n5 = 5;
+
+let somma = (n1+n2+n3+n4+n5);
+
+console.log( " la somma tra i numeri equivale a " + somma);
+
+let media = (somma / 5);
+
+console.log( " La media equivale a " + media);
